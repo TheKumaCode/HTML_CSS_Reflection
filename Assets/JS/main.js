c1c2b68d2ddc676f36cc0173993ee5beb84d7c0e
RJURU <110449011+RJURU@users.noreply.github.com>
2022-09-14 11:25:35 +0100

    Sticky header broken - for peer testing

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -2,25 +2,34 @@ const header = document.getElementById('header')
 const main = document.querySelector('.main')
 const body = document.getElementById('body')
 const sidebar = document.querySelector('.sidebar')
-const closeSidebar = document.querySelector('.sidebar-open')
+const openedSidebar = document.querySelector('.sidebar-open')
 const navBtn = header.querySelector('.btn-nav')
 
-navBtn.addEventListener('click', () => {
+$(document).ready(function () {
+    navBtn.addEventListener('click', (e) => {
+        openSidebar(e)
+    })
+
+    openedSidebar.addEventListener('click', () => {
+        closeSidebar()
+    })
+})
+
+function openSidebar(e) {
     body.style.overflow = "hidden";
-    closeSidebar.style.display = "block"
+    openedSidebar.style.display = "block"
     sidebar.style.zIndex = "5"
     main.classList.add('disableMain')
-    event.target.classList.add('sidebarOn')
-})
+    e.target.classList.add('sidebarOn')
+}
 
-closeSidebar.addEventListener('click', () => {
+function closeSidebar() {
     body.removeAttribute('style')
     sidebar.removeAttribute('style')
-    closeSidebar.style.display = "none"
+    openedSidebar.style.display = "none"
     main.classList.remove('disableMain')
     navBtn.classList.remove('sidebarOn')
-})
-
+}
 
 $('.owl-carousel').owlCarousel({
     loop: true,
@@ -32,3 +41,18 @@ $('.owl-carousel').owlCarousel({
     autoplayPauseOnHover: true
 })
 
+var lastPosition = 0;
+const mainHeader = document.getElementById('header')
+const stickyHeader = document.getElementById('sticky-header')
+
+document.addEventListener('scroll', () => {
+    let position = window.scrollY
+    if (position < lastPosition && position >= 168) {
+        stickyHeader.style.display = "block"
+    } else if (position > lastPosition) {
+        stickyHeader.style.display = "none"
+    } else {
+        return;
+    }
+    lastPosition = position <= 0 ? 0 : position
+})
