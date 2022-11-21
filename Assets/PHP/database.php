bb4c7570f82a5c04ae964ead6c21e49f230d4677
RJURU <110449011+RJURU@users.noreply.github.com>
2022-11-21 12:15:29 +0000

    Database change

--- Assets/PHP/database.php
+++ Assets/PHP/database.php
@@ -1,14 +1,14 @@
 <?php 
 
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "netmatters";
-
 // $dbServerName = "localhost";
-// $dbUsername = "robertas_vildziunas";
-// $dbPassword = "8H6KgUo0uU~$";
-// $dbName = "robertas_netmatters";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "netmatters";
+
+$dbServerName = "localhost";
+$dbUsername = "robertas_vildziunas";
+$dbPassword = "8H6KgUo0uU~$";
+$dbName = "robertas_netmatters";
 
 $connect = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
 
