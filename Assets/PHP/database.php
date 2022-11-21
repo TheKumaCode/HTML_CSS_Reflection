68cd0432f6005b6a2f9a2f4f07e36daf7b5ecca0
RJURU <110449011+RJURU@users.noreply.github.com>
2022-11-21 08:38:53 +0000

    Typo

--- Assets/PHP/database.php
+++ Assets/PHP/database.php
@@ -1,9 +1,14 @@
 <?php 
 
+// $dbServerName = "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "netmatters";
+
 $dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "netmatters";
+$dbUsername = "robertas_vildziunas";
+$dbPassword = "8H6KgUo0uU~$";
+$dbName = "robertas_netmatters";
 
 $connect = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
 
